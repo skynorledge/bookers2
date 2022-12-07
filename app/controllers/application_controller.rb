c1518b9a75f231098c1c09781f87571d1310f537
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2022-12-07 18:55:02 +0000

    [update3]bookers2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    homes_about_path
+    books_path
   end
 
 
   def after_sign_out_path_for(resource)
-    homes_about_path
+    "/"
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
+
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+
     @user= User.find(params[:id])
+
   end
 
   def update
@@ -20,10 +25,23 @@ class UsersController < ApplicationController
       @users = User.all
       render :edit
     end
+
   end
 
+private
+
   def user_params
+
     params.require(:user).permit(:name, :profile_image)
+
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to books_path
+    end
   end
 
 end
